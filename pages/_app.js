f668f27e75fe108ada8a5040d353cf6bf8839e9c
Brian McIlwain <technicallyemployed@gmail.com>
2023-02-18 15:46:14 -0700

    Ran prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,98 +1,98 @@
-import '@/styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 import {
   RainbowKitProvider,
   getDefaultWallets,
   connectorsForWallets,
-} from '@rainbow-me/rainbowkit';
-import {createClient, configureChains, WagmiConfig} from 'wagmi';
-import {publicProvider} from 'wagmi/providers/public';
-import {argentWallet, trustWallet} from '@rainbow-me/rainbowkit/wallets';
+} from "@rainbow-me/rainbowkit";
+import { createClient, configureChains, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { argentWallet, trustWallet } from "@rainbow-me/rainbowkit/wallets";
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 const calypsoChain = {
-  name: 'SKALE | Calypso NFT Hub',
-  network: 'SKALE',
+  name: "SKALE | Calypso NFT Hub",
+  network: "SKALE",
   testnet: !isProduction,
   nativeCurrency: {
     decimals: 18,
-    symbol: 'sFUEL',
-    name: 'sFUEL',
+    symbol: "sFUEL",
+    name: "sFUEL",
   },
-  id: parseInt(isProduction ? '0x5d456c62' : '0x1482a7b2'),
+  id: parseInt(isProduction ? "0x5d456c62" : "0x1482a7b2"),
   rpcUrls: {
     default: {
       http: [
         isProduction
-          ? 'https://mainnet.skalenodes.com/v1/honorable-steel-rasalhague'
-          : 'https://staging-v3.skalenodes.com/v1/staging-utter-unripe-menkar',
+          ? "https://mainnet.skalenodes.com/v1/honorable-steel-rasalhague"
+          : "https://staging-v3.skalenodes.com/v1/staging-utter-unripe-menkar",
       ],
     },
   },
 };
 
 const europaChain = {
-  name: 'SKALE | Europa Liquidity Hub',
-  network: 'SKALE',
+  name: "SKALE | Europa Liquidity Hub",
+  network: "SKALE",
   testnet: !isProduction,
   nativeCurrency: {
     decimals: 18,
-    symbol: 'sFUEL',
-    name: 'sFUEL',
+    symbol: "sFUEL",
+    name: "sFUEL",
   },
-  id: parseInt(isProduction ? '0x79f99296' : '0x1c6199cc'),
+  id: parseInt(isProduction ? "0x79f99296" : "0x1c6199cc"),
   rpcUrls: {
     default: {
       http: [
         isProduction
-          ? 'https://mainnet.skalenodes.com/v1/elated-tan-skat'
-          : 'https://staging-v3.skalenodes.com/v1/staging-legal-crazy-castor',
+          ? "https://mainnet.skalenodes.com/v1/elated-tan-skat"
+          : "https://staging-v3.skalenodes.com/v1/staging-legal-crazy-castor",
       ],
     },
   },
 };
 
 const ethereum = {
-  name: 'Ethereum',
-  network: 'Ethereum Mainnet',
+  name: "Ethereum",
+  network: "Ethereum Mainnet",
   testnet: !isProduction,
   nativeCurrency: {
     decimals: 18,
-    symbol: 'ETH',
-    name: 'Ether',
+    symbol: "ETH",
+    name: "Ether",
   },
   id: isProduction ? 1 : 5,
   rpcUrls: {
     default: {
       http: [
         isProduction
-          ? 'https://eth.llamarpc.com'
-          : 'https://goerli.blockpi.network/v1/rpc/public',
+          ? "https://eth.llamarpc.com"
+          : "https://goerli.blockpi.network/v1/rpc/public",
       ],
     },
   },
 };
 
-const {chains, provider, webSocketProvider} = configureChains(
+const { chains, provider, webSocketProvider } = configureChains(
   [ethereum, calypsoChain, europaChain],
   [publicProvider()]
 );
 
-const {wallets} = getDefaultWallets({
-  appName: 'NFT Sale',
+const { wallets } = getDefaultWallets({
+  appName: "NFT Sale",
   chains,
 });
 
 const demoAppInfo = {
-  appName: 'NFT Sale',
+  appName: "NFT Sale",
 };
 
 const connectors = connectorsForWallets([
   ...wallets,
   {
-    groupName: 'Other',
-    wallets: [argentWallet({chains}), trustWallet({chains})],
+    groupName: "Other",
+    wallets: [argentWallet({ chains }), trustWallet({ chains })],
   },
 ]);
 
@@ -103,7 +103,7 @@ const wagmiClient = createClient({
   webSocketProvider,
 });
 
-export default function App({Component, pageProps}) {
+export default function App({ Component, pageProps }) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,54 @@
-import Head from 'next/head';
-import styles from '@/styles/Home.module.css';
-import {ConnectButton} from '@rainbow-me/rainbowkit';
-import React, {useEffect} from 'react';
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React, { useEffect } from "react";
 
 export default function Home() {
   const [metaport, setMetaport] = React.useState();
 
   async function loadMetaport() {
-    const Metaport = (await import('@skalenetwork/metaport')).Metaport;
+    const Metaport = (await import("@skalenetwork/metaport")).Metaport;
 
     const _metaport = new Metaport({
-      mainnetEndpoint: 'https://eth-goerli.public.blastapi.io',
+      mainnetEndpoint: "https://eth-goerli.public.blastapi.io",
       openOnLoad: false, // Open Metaport on load (optional, default = false)
       openButton: true, // Show open/close action button (optional, default = true)
       autoLookup: false, // Automatic token lookup for M2S tokens (default = true)
-      skaleNetwork: 'staging3', // CALYPSO_DETAILS.skaleNetworkName, //ENVIRONMENT, //"staging3", // SKALE network that will be used - mainnet or staging (optional, defualt = mainnet)
+      skaleNetwork: "staging3", // CALYPSO_DETAILS.skaleNetworkName, //ENVIRONMENT, //"staging3", // SKALE network that will be used - mainnet or staging (optional, defualt = mainnet)
       chains: [
-        'mainnet',
-        'staging-legal-crazy-castor',
-        'staging-utter-unripe-menkar',
+        "mainnet",
+        "staging-legal-crazy-castor",
+        "staging-utter-unripe-menkar",
       ],
       chainsMetadata: {
         // Chain name aliases that will be displayed in the UI (optional, defualt = {})
-        'staging-legal-crazy-castor': {
-          alias: 'Europa', // optional
-          minSfuelWei: '0.0001', // optional
+        "staging-legal-crazy-castor": {
+          alias: "Europa", // optional
+          minSfuelWei: "0.0001", // optional
         },
-        'staging-utter-unripe-menkar': {
-          alias: 'Calypso',
-          minSfuelWei: '0.0001', // optional
+        "staging-utter-unripe-menkar": {
+          alias: "Calypso",
+          minSfuelWei: "0.0001", // optional
         },
       },
       tokens: {
         mainnet: {
           eth: {
-            chains: ['staging-legal-crazy-castor'],
+            chains: ["staging-legal-crazy-castor"],
           },
         },
-        'staging-legal-crazy-castor': {
+        "staging-legal-crazy-castor": {
           erc20: {
             ETHC: {
               // wrapper token
-              address: '0xa270484784f043e159f74C03B691F80B6F6e3c24', // wrapper token address
-              name: 'ETH', // wrapper token display name
-              symbol: 'ETHC', /// TODO -> THis must match tokenKeyName
+              address: "0xa270484784f043e159f74C03B691F80B6F6e3c24", // wrapper token address
+              name: "ETH", // wrapper token display name
+              symbol: "ETHC", /// TODO -> THis must match tokenKeyName
               wraps: {
                 // token that needs to be wrapped
-                address: '0xD2Aaa00700000000000000000000000000000000', // unwrapped token address
-                symbol: 'ETH', // unwrapped token symbol
-                iconUrl: '', // optional, icon URL for the origin token
+                address: "0xD2Aaa00700000000000000000000000000000000", // unwrapped token address
+                symbol: "ETH", // unwrapped token symbol
+                iconUrl: "", // optional, icon URL for the origin token
               },
             },
           },
@@ -56,9 +56,9 @@ export default function Home() {
       },
       theme: {
         // custom widget theme (default = dark SKALE theme)
-        primary: '#E37B26', // primary accent color for action buttons
-        background: '#fffbf7', // background color
-        mode: 'light', // theme type - dark or light
+        primary: "#E37B26", // primary accent color for action buttons
+        background: "#fffbf7", // background color
+        mode: "light", // theme type - dark or light
       },
     });
 
@@ -73,11 +73,11 @@ export default function Home() {
 
   const handleTranser = () => {
     metaport.transfer({
-      amount: '1000000000000',
-      chains: ['staging-legal-crazy-castor', 'staging-utter-unripe-menkar'],
-      tokenType: 'erc20',
+      amount: "1000000000000",
+      chains: ["staging-legal-crazy-castor", "staging-utter-unripe-menkar"],
+      tokenType: "erc20",
       lockValue: false,
-      tokenKeyname: '_ETHC_0xa270484784f043e159f74C03B691F80B6F6e3c24',
+      tokenKeyname: "_ETHC_0xa270484784f043e159f74C03B691F80B6F6e3c24",
     });
   };
 
